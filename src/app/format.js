a2293351d53a717766a2d9463191d14e2350c44d
AmandaAbarte <amanda.abarte@gmail.com>
2023-04-02 23:45:07 +0200

    fix check if date is valid

--- src/app/format.js
+++ src/app/format.js
@@ -1,19 +1,26 @@
 export const formatDate = (dateStr) => {
-  const date = new Date(dateStr)
-  const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
-  const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(date)
-  const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
-  const month = mo.charAt(0).toUpperCase() + mo.slice(1)
-  return `${month.substr(0,3)}. ${parseInt(da)}, ${ye.toString().substr(0,4)}`
-}
- 
+  const date = new Date(dateStr);
+  if (dateIsValid(date)) {
+    const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+    const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(date);
+    const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+    const month = mo.charAt(0).toUpperCase() + mo.slice(1);
+    return `${month.substr(0, 3)}. ${parseInt(da)}, ${ye
+      .toString()
+      .substr(0, 4)}`;
+  }
+};
+
 export const formatStatus = (status) => {
   switch (status) {
     case "pending":
-      return "Pending"
+      return "Pending";
     case "accepted":
-      return "Accepted"
+      return "Accepted";
     case "refused":
-      return "Cancelled"
+      return "Cancelled";
   }
+};
+function dateIsValid(date) {
+  return !Number.isNaN(new Date(date).getTime());
 }
